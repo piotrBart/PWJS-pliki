ae894d5e6fe264901267408072294616f34a6404
piotrBart <44523802+piotrBart@users.noreply.github.com>
2018-10-27 14:21:02 +0200

    Add files via upload

---
+++ problem007.js
@@ -0,0 +1,11 @@
+function findMax()
+{var max=arguments[0];
+  for(var i = arguments.length-1; i>0; i--)
+  {
+    if(arguments[i]>max)
+      {
+        max=arguments[i];        
+      }
+  }
+  return max;
+}
